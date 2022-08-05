7bc6f89176ae72843d8a4f2144eae92e8485f8c3
GregBullington <gbullington23@gmail.com>
2022-08-05 16:59:14 -0600

    environment-test

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('Your code goes here...');
+//testing environment
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 10;
+const number2 = 20;
+
+const result = add(number1, number2);
+console.log(result);
